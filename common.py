6b0448e82409b14146034e3bde8421165dcadafa
Eli Lipsitz <eli.lipsitz@gmail.com>
2018-12-06 16:25:19 -0800

    allow using our on homograpfinder

--- common.py
+++ common.py
@@ -14,6 +14,7 @@ import tempfile
 import io
 import os
 from multiprocessing import Pool
+import random
 
 def load_images_from_directory(directory, scale=1.0):
     filenames = sorted(os.listdir(directory))
@@ -71,4 +72,59 @@ def vecdist(a, b):
 
 #######################################################################
 
+# RANSAC
 
+def warp_point(H, pt):
+    x, y, p = H @ (pt[0], pt[1], 1.0)
+    return (x / p, y / p)
+
+def get_homography(pts_0, pts_1, max_iterations=1000, dist_threshold=5):
+    assert len(pts_0) == len(pts_1)
+    m = len(pts_0)
+    best_inliers = None
+    for i in range(max_iterations):
+        rand_inds = random.sample(range(m), 4)
+        pts0 = [pts_0[x] for x in rand_inds]
+        pts1 = [pts_1[x] for x in rand_inds]
+        H = compute_hom_rigid(pts0, pts1, inv=True)
+        inliers = []
+        for j in range(m):
+            pt0_warp = warp_point(H, pts_0[j])
+            pt1 = pts_1[j]
+            d = vecdist(pt0_warp, pt1)
+            if d < dist_threshold:
+                inliers.append(j)
+
+        if best_inliers is None or len(inliers) > len(best_inliers):
+            best_inliers = inliers
+
+    pts0 = [pts_0[x] for x in best_inliers]
+    pts1 = [pts_1[x] for x in best_inliers]
+    H = compute_hom_rigid(pts0, pts1, inv=False)
+    print("RANSAC returned", len(best_inliers), "/", m)
+    return H
+
+def compute_hom_rigid(im1_pts, im2_pts, m=1.0, inv=True):
+    # http://nghiaho.com/?p=2208
+    # ax - by + 1c + 0d= x'
+    # ay + by + 0c + 1d = y'
+    assert len(im1_pts) == len(im2_pts)
+    
+    A = []
+    b = []
+    for i in range(len(im1_pts)):
+        if inv:
+            x, y = im1_pts[i]
+            X, Y = im2_pts[i]
+        else:
+            y, x = im1_pts[i]
+            Y, X = im2_pts[i]
+        x, y, X, Y = x * m, y * m, X * m, Y * m
+        A.append([x, -y, 1.0, 0.0])
+        A.append([y,  x, 0.0, 1.0])
+        b.append(X)
+        b.append(Y)
+        
+    x, _, _, _  = np.linalg.lstsq(np.float32(A), np.float32(b), rcond=None)
+    H = np.float32([[x[0], -x[1], x[2]], [x[1], x[0], x[3]], [0.0, 0.0, 1.0]])
+    return H
